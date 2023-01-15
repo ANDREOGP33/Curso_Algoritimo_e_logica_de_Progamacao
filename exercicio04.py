cce3009a8ac96b2afe6cd943bcd8121072c7e389
ANDREOGP33 <andregasparoni690@gmail.com>
2023-01-15 13:11:42 -0300

    Solução do problema crescente

---
+++ exercicio04.py
@@ -0,0 +1,15 @@
+x = int
+y = int
+
+x = input("Digite o primeiro numero: ")
+y = input ("Digite o segundo numero: ")
+
+while x != y:
+    if  x > y: 
+        print("DECRESCENTE!")
+    else:
+        print("CRESCENTE!")  
+          
+    x = input("Digite o primeiro numero: ")
+    y = input ("Digite o segundo numero: ")    
+
