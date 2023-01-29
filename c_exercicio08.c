e417a4b004e755c3e962c0a1d71b9437fb6a3def
ANDREOGP33 <andregasparoni690@gmail.com>
2023-01-28 22:17:50 -0300

    200~Solução do problema media_idade

--- c_exercicio08.c
+++ c_exercicio08.c
@@ -3,29 +3,31 @@
 int mais()
 {
     int idade, total;
-    double media;
-
+    double media, soma;
 
     printf("Digite as idades: \n");
-
     idade = 0;
     total = 0;
-
-    scanf("%d\n", idade);
+    scanf("%d\n", &idade);
     total = total + 1;
-    
-    if (idade < 0) exit(0);
-    if (idade >= 0)
-    {
-        scanf("%d\n", idade);
-        total = total + 1;
 
-    }
+    media = 0;
+    soma = 0;
+
+    if (idade < 0)
+    {
+        printf("IMPOSSIVEL CALCULAR");
+    }   
     else
     {
-        
+        scanf("%d\n",&idade);
+        total = total + 1; 
+        soma = soma + idade;
     }
 
+    media = soma / total;
+    printf("Media das idades: ");
+    scanf("%lf", &media);
 
     return 0;
 }
