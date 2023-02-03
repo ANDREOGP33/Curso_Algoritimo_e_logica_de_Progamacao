748ac22b4c242ab7badbc53c2c57743696c841bf
ANDREOGP33 <andregasparoni690@gmail.com>
2023-02-02 21:12:54 -0300

    Solução do problema alturas

--- c_exercicio14.c
+++ c_exercicio14.c
@@ -1,26 +1,60 @@
 # include <stdio.h>
-# include <string.h>
-
-void limpar_entrada() {
- char c;
- while ((c = getchar()) != '\n' && c != EOF) {}
-}
 
 int main()
 {
-    int n, i, idade[n];
-    double altura[n];
-    char nome[n];
-    
-    printf("Quantas pessoas vc vai digitar: ");
-    scanf("%d", n);
+    int n;
 
-    for (i = 0; i >= n; i++)
+    printf("Quantas pessoas vc vai digitar: ");
+    scanf("%d", &n);
+
+    int i, idade[n];
+    double altura[n];
+    char nome[n][50];
+
+    for (i = 0; i < n; i++)
     {
-        limpar_entrada();
-        fgets(char, nome[i], stdin);
+        printf("Dados da %da pessoa:\n", i + 1);
+        printf("Nome: ");
+        fseek(stdin, 0, SEEK_END);
+        gets(nome[i]);
+        printf("Idade: ");
         scanf("%d", &idade[i]);
+        printf("Altura: ");
         scanf("%lf", &altura[i]);
+    }
+
+    int idadetotal, m16;
+
+    idadetotal = 0;
+    for (i = 0; i < n; i++)
+    {
+        idadetotal = idadetotal + idade[i];
+    }
+    
+    double media;
+
+    media = idadetotal / n;
+
+    printf("Media das idades: %.2lf\n", media);
+
+    m16 = 0;
+
+    for (i = 0; i < n; i++)
+    {
+        if (idade[i] < 16)
+        {
+            m16 = m16 + 1;
+            printf("Menores de 16 anos: %s\n", nome[i]);
+        }
+    }
+
+    double pm16;
+
+    pm16 = (m16 * 100) / n;
+
+    if ( m16 > 0)
+    {
+        printf("Porcentagem de pessoas menores de 16 anos: %.2lf", pm16);
     }
 
 
