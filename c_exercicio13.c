b400549dcf83303397aaea5add0a09b4e910844f
ANDREOGP33 <105257924+ANDREOGP33@users.noreply.github.com>
2023-02-02 23:48:02 -0300

    Update c_exercicio13.c

--- c_exercicio13.c
+++ c_exercicio13.c
@@ -1,8 +1,62 @@
-# include<stdio.h>
+# include <stdio.h>
 
 int main()
 {
+    int n;
+
+    printf("Quantas pessoas vc vai digitar: ");
+    scanf("%d", &n);
+
+    int i, idade[n];
+    double altura[n];
+    char nome[n][50];
+
+    for (i = 0; i < n; i++)
+    {
+        printf("Dados da %da pessoa:\n", i + 1);
+        printf("Nome: ");
+        fseek(stdin, 0, SEEK_END);
+        gets(nome[i]);
+        printf("Idade: ");
+        scanf("%d", &idade[i]);
+        printf("Altura: ");
+        scanf("%lf", &altura[i]);
+    }
+
+    int idadetotal, m16;
+
+    idadetotal = 0;
+    for (i = 0; i < n; i++)
+    {
+        idadetotal = idadetotal + idade[i];
+    }
     
+    double media;
+
+    media = idadetotal / n;
+
+    printf("Media das idades: %.2lf\n", media);
+
+    m16 = 0;
+
+    for (i = 0; i < n; i++)
+    {
+        if (idade[i] < 16)
+        {
+            m16 = m16 + 1;
+            printf("Menores de 16 anos: %s\n", nome[i]);
+        }
+    }
+
+    double pm16;
+
+    pm16 = (m16 * 100) / n;
+
+    if ( m16 > 0)
+    {
+        printf("Porcentagem de pessoas menores de 16 anos: %.2lf", pm16);
+    }
+
 
     return 0;
 }
