80fb6e974afdd4788d1a7d26c7c53ef7805a9732
ANDREOGP33 <105257924+ANDREOGP33@users.noreply.github.com>
2023-02-05 23:17:15 -0300

    Solução do problema "diagonal_negativos"

--- c_exercicio14.c
+++ c_exercicio14.c
@@ -2,22 +2,42 @@
 
 int main()
 {
-    int n, m,i ,j;
+    int n, m,i ,j, negativo;
     
     printf("Qual a ordem da matriz? ");
     scanf("%d", &n);
     m = n;
     
-    int vetor[n][m];
+    int matriz[n][m];
     
     for (i = 0; i < n; i++)
     {
         for (j = 0; j < m; j++)
         {
             printf("Elemento; [%d,%d]: ", i,j);
-            scanf("")
+            scanf("%d", &matriz[i][j]);
         }
     }
+
+    printf("Diagonal Principal: ");
+    for (i = 0; i < n; i++)
+    {
+        printf("%d\n", matriz[i][i]);
+    }
+
+    negativo = 0;
+    printf("Quantidade de negaivo: ");
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < m; j++)
+        {
+            if (matriz[i][j] < 0)
+            {
+                negativo = negativo + 1;
+            }
+        }
+    }
+    printf("%d", negativo);
 
     return 0;
 }
