5cf81d24f07131ba3f99dd275b83642ed1050a48
ANDREOGP33 <andregasparoni690@gmail.com>
2023-02-10 08:14:41 -0300

    Solução do problema crescente

---
+++ cpp_exercicio04.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n1, n2;
+    
+    cout << "Digite dois numeros; \n";
+    cin >> n1;
+    cin >> n2;
+    
+    
+    
+    if (n1 > n2)
+    {
+        cout << "DECRESCENTE!";
+    }
+    else
+    {
+        cout << "CRESCENTE!";
+    }
+    
+    
+    return 0;
+}
